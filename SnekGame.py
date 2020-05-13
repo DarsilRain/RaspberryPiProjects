067d169482ed169ce347c2ba543493528ddfe212
DarsilRain <64807481+DarsilRain@users.noreply.github.com>
2020-05-12 20:24:22 -0500

    Add files via upload
    Got the snake to eat the mommies. Now get it to grow when it eats one: "Growing and speeding up"

--- SnekGame.py
+++ SnekGame.py
@@ -1,5 +1,5 @@
 """ TODO
-Get snek to eat nommies
+Get snek to grow when it eats the nommies
 
 """
 from sense_hat import SenseHat
@@ -30,42 +30,44 @@ def draw_snek():
         sense.set_pixel(point[0], point[1], white)
 
 def move(direction):
+    remove = True
     global score
     last= snek[-1] # the last value in the snek array: the snake head
     first=snek[0]  # the first or 0 value in the snek array
     next=list(last)
-    
+   
     if direction == "right":
         next[0]=last[0] + 1
         if next[0]== 8:
             next[0] = 0
-    
+            
     if direction == "left":
         next[0]=last[0] - 1
         if next[0]== -1:
-            next[0] = 7
-    
+          next[0] = 7
+  
     if direction == "up":
-        next[1]=last[1] - 1
-        if next[1]== -1:
-            next[1] = 7
- 
+      next[1]=last[1] - 1
+      if next[1]== -1:
+          next[1] = 7
+   
     if direction == "down":
-        next[1]=last[1] + 1
-        if next[1]== 8:
-            next[1] = 0
-
-    sense.set_pixel(next[0], next[1], white) # draw the new part of snek (x,y, color)
-    snek.append(next)
-    sense.set_pixel(first[0], first[1], blank) # remove old tail position (x,y,color)
-    snek.remove(first)
-    
-    if next in nommies:
-        print(next)
-        nommies.remove(nom)
-        score=score+1
-        print("snek got a nom!")
+      next[1]=last[1] + 1
+      if next[1]== 8:
+          next[1] = 0
+   
+    if remove == True:
+      sense.set_pixel(next[0], next[1], white) # draw the new part of snek (x,y, color)
+      snek.append(next)
+      sense.set_pixel(first[0], first[1], blank) # remove old tail position (x,y,color)
+      snek.remove(first)
         
+    if next in nommies:
+      print(next)
+      nommies.remove(next)
+      score=score+1
+      print("snek got a nom!")
+            
         
 def pushed_up(event):
     global direction
